5fd1964aa623aba673798f5fe30daf7c4ff9467e
Silas Ferraciolli Corrêa <silascofc@gmail.com>
2017-06-21 00:15:45 -0300

    Iniciando modelagem de aplicação de condomínio.

--- mirantedupark/models.py
+++ mirantedupark/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    """Pessoa.
+
+    Classe genérica aproveitada por outras
+
+    """
+
+    class Meta:
+        pass
+
+    def __str__():
+        pass
+
+
+class NaturalPerson(Person):
+    """Pessoa Física.
+
+    """
+
+    class Meta:
+        pass
+
+    def __str__():
+        pass
+
+
+class LegalPerson(Person):
+    """Pessoa Jurídica.
+
+    """
+
+    class Meta:
+        pass
+
+    def __str__():
+        pass
+
+
+class Owner(models.Model):
+    """Condômino.
+
+    """
+
+    class Meta:
+        pass
+
+    def __str__():
+        pass
+
+
+class Tenant(models.Model):
+    """Inquilino.
+
+    """
+
+    pass
+
+    class Meta:
+        pass
+
+    def __str__():
+        pass
