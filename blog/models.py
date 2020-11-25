3fde34e927d7e9f7bde6cf6bf002b88c75bca4bd
Silas Ferraciolli Corrêa <silascofc@gmail.com>
2020-11-25 23:20:08 +0000

    Updating settings.conf version info. Shortening super call of PublishedManager

--- blog/models.py
+++ blog/models.py
@@ -31,7 +31,7 @@ for item in range(1,41):
 class PublishedManager(models.Manager):
 
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
